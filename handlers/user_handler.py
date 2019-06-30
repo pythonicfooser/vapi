639e84f31ceb7fbe19ed0e8b28472dc32a43857f
fooser <fooser@foosers-Air.lan>
2019-06-30 16:57:04 +0200

    Added tox and Docker data for easier deployments.

--- handlers/user_handler.py
+++ handlers/user_handler.py
@@ -1,9 +1,10 @@
-from aiohttp.web import Response 
+from aiohttp.web import Response
 from jsonschema import validate
 from models import user_schema, query_schema, increase_amount_schema
 from jsonschema.exceptions import ValidationError
 from json import dumps
 from json.decoder import JSONDecodeError
+
 
 async def set_user(request):
     """ Method for adding a user.
@@ -15,19 +16,26 @@ async def set_user(request):
     try:
         input_data = await request.json()
         validate(input_data, user_schema)
-        exist_user = await request.app['db']['users'].find_one({"username": input_data['username']})
+        exist_user = await request.app['db']['users'].find_one(
+            {"username": input_data['username']})
         if exist_user:
-            return  Response(body=dumps({"status": "User already exists"}), content_type="application/json")
+            return Response(body=dumps({"status": "User already exists"}),
+                            content_type="application/json")
 
         input_data['amount'] = 0
         request.app['db']['users'].insert_one(input_data)
-        return Response(body=dumps({"status": "User added"}), content_type="application/json")
+        return Response(body=dumps({"status": "User added"}),
+                        content_type="application/json")
     except ValidationError:
-        return Response(body=dumps({"status": "Error in sent fields"}), content_type="application/json")
+        return Response(body=dumps({"status": "Error in sent fields"}),
+                        content_type="application/json")
     except JSONDecodeError:
-        return Response(body=dumps({"status": "Format error"}), content_type="application/json") 
+        return Response(body=dumps({"status": "Format error"}),
+                        content_type="application/json")
     except Exception:
-        return Response(body=dumps({"Error": "Uh oh... Something went wrong! We are working on it!"}), content_type="application/json") 
+        return Response(body=dumps(
+            {"Error": "Uh oh... Something went wrong! We are working on it!"}),
+                        content_type="application/json")
 
 
 async def get_user_balance(request):
@@ -39,43 +47,62 @@ async def get_user_balance(request):
     try:
         input_data = await request.json()
         validate(input_data, query_schema)
-        exist_user = await request.app['db']['users'].find_one({"username": input_data['username']})
+        exist_user = await request.app['db']['users'].find_one(
+            {"username": input_data['username']})
         if not exist_user:
-            return Response(body=dumps({"status": "User not exists"}), content_type="application/json") 
- 
-        return Response(body=dumps({"amount": exist_user['amount']}), content_type="application/json")
+            return Response(body=dumps({"status": "User not exists"}),
+                            content_type="application/json")
+
+        return Response(body=dumps({"amount": exist_user['amount']}),
+                        content_type="application/json")
     except ValidationError:
-        return Response(body=dumps({"status": "Error in sent fields"}), content_type="application/json")
+        return Response(body=dumps({"status": "Error in sent fields"}),
+                        content_type="application/json")
 
     except JSONDecodeError:
-        return Response(body=dumps({"status": "Format error"}), content_type="application/json") 
+        return Response(body=dumps({"status": "Format error"}),
+                        content_type="application/json")
     except Exception:
-        return Response(body=dumps({"Error": "Uh oh... Something went wrong! We are working on it!"}), content_type="application/json")
+        return Response(body=dumps(
+            {"Error": "Uh oh... Something went wrong! We are working on it!"}),
+                        content_type="application/json")
+
 
 async def increase_user_balance(request):
     """ Method for increasing user balance.
          Must receive, via JSON:
          - username
          - amount
-    """    
+    """
     try:
         input_data = await request.json()
         validate(input_data, increase_amount_schema)
         if input_data['amount'] <= 0:
-            return Response(body=dumps({"status": "Invalid amount"}), content_type="application/json") 
+            return Response(body=dumps({"status": "Invalid amount"}),
+                            content_type="application/json")
 
-        exist_user = await request.app['db']['users'].find_one({"username": input_data['username']})
+        exist_user = await request.app['db']['users'].find_one(
+            {"username": input_data['username']})
         if not exist_user:
-            return Response(body=dumps({"status": "User not exists"}), content_type="application/json") 
+            return Response(body=dumps({"status": "User not exists"}),
+                            content_type="application/json")
 
         new_amount = exist_user['amount'] + input_data['amount']
-        await request.app['db']['users'].update_one({"username": input_data['username']}, {'$set': {"amount": new_amount }}) 
-        return Response(body=dumps({"message": 'amount updated'}), content_type="application/json")
+        await request.app['db']['users'].update_one(
+            {"username": input_data['username']},
+            {'$set': {
+                "amount": new_amount
+            }})
+        return Response(body=dumps({"message": 'amount updated'}),
+                        content_type="application/json")
     except ValidationError:
-        return Response(body=dumps({"status": "Error in sent fields"}), content_type="application/json")
+        return Response(body=dumps({"status": "Error in sent fields"}),
+                        content_type="application/json")
 
     except JSONDecodeError:
-        return Response(body=dumps({"status": "Format error"}), content_type="application/json") 
+        return Response(body=dumps({"status": "Format error"}),
+                        content_type="application/json")
     except Exception:
-        return Response(body=dumps({"Error": "Uh oh... Something went wrong! We are working on it!"}), content_type="application/json")
-
+        return Response(body=dumps(
+            {"Error": "Uh oh... Something went wrong! We are working on it!"}),
+                        content_type="application/json")
--- utils/router.py
+++ utils/router.py
@@ -1,13 +1,12 @@
 from aiohttp import web
-from handlers import * 
+from handlers import set_user, get_user_balance,\
+                     increase_user_balance, transfer
+
 
 def set_routes(app):
     app.add_routes([
-        web.get('/', hello),
-        web.post('/post_messages', post_messages),
         web.post('/add_user', set_user),
         web.post('/get_user_balance', get_user_balance),
         web.post('/increase_user_balance', increase_user_balance),
         web.post('/transfer', transfer)
     ])
-    
